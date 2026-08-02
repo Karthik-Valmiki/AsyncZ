d3df63fa21a6efdaa78275cf62b9ccc938bdbe26
Karthik-Valmiki <valmikikarthik07@gmail.com>
2026-08-02 13:44:22 +0530

    chore: Test with real traffic data

--- tests/load_test.js
+++ tests/load_test.js
@@ -4,14 +4,19 @@ import { check, sleep } from 'k6';
 // 1. Configuration — 5 jobs/sec per user (sleep 0.2s), 10 VUs = 50 jobs/sec total
 // On Windows, keep VUs ≤ 10 to avoid the 512 socket limit crash.
 // On Linux/WSL2 you can safely push to 100+ VUs.
+//docker-compose run --rm k6 run /tests/load_test.js
+
 export const options = {
-    vus: 500,          // 10 concurrent users
-    duration: '10s',  // Run long enough to see queue backlog
+    vus: 5000,          // 10 concurrent users
+    duration: '20s',  // Run long enough to see queue backlog
 };
 
 // 2. The Loop
 export default function () {
-    const url = 'http://localhost:8000/jobs';
+    // const url = 'http://localhost:8000/jobs'; # for windows
+    // const url = 'http://host.docker.internal:8000/jobs';   // for docker
+    const url = 'http://api:8000/jobs';
+
 
     // The exact format your schemas.py expects
     const payload = JSON.stringify({
